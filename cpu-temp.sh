bc7d87f62cdf44d4c1800c3a9adbf903872cb0b3
daneenjah <kd7nfr@yahoo.com>
2022-07-28 15:41:50 -0400

    Fix cpu-temp

--- cpu-temp.sh
+++ cpu-temp.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-sensors | grep "Package id 0:" | tr -d '+' | awk '{print $4}' | sed 's/.0//g'
+#sensors | grep "Package id 0:" | tr -d '+' | awk '{print $4}' | sed 's/.0//g'
+
+sensors | grep "Package id 0:" | tr -d '+' | awk '{print substr($4, 1, length($0)-52)}'
