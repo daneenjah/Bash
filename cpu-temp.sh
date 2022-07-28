7154874c7e06e17d332e786d5abb38a44968e3c5
daneenjah <kd7nfr@yahoo.com>
2022-07-28 15:44:57 -0400

    Fix cpu-temp formatting

--- cpu-temp.sh
+++ cpu-temp.sh
@@ -2,4 +2,6 @@
 
 #sensors | grep "Package id 0:" | tr -d '+' | awk '{print $4}' | sed 's/.0//g'
 
-sensors | grep "Package id 0:" | tr -d '+' | awk '{print substr($4, 1, length($0)-52)}'
+temp=$(sensors | grep "Package id 0:" | tr -d '+' | awk '{print substr($4, 1, length($0)-52)}')
+
+echo $temp°C
